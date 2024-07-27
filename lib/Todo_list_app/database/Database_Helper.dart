2667018d0824b1c869f4efb9be896ebeca73bc7b
MEetpanchal1222 <panchalmeetr97@gmail.com>
2024-07-27 19:59:36 +0530

    27/7/24

--- lib/Todo_list_app/database/Database_Helper.dart
+++ lib/Todo_list_app/database/Database_Helper.dart
@@ -2,21 +2,41 @@ import 'package:getx_programing/Todo_list_app/modal/todo_modal.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+/* Ensures that there's only one instance of DatabaseHelper across the entire app.*/
+
 class DatabaseHelper {
+  //Singleton Pattern
+  //Declares a variable named _instance of type DatabaseHelper;
+  //This part initializes _instance with a new DatabaseHelper object.
+  // The _internal() constructor is a named private constructor used to create the instance.
   static final DatabaseHelper _instance = DatabaseHelper._internal();
+
+  //when the  class call the it returns the existing _instance created earlier.
+  // This ensures that only one instance of DatabaseHelper is used throughout the app
   factory DatabaseHelper() => _instance;
+
+  // _database: A static variable to hold the instance of the SQFLite database.
   static Database? _database;
 
+  //_internal is a named private constructor used to create the instance of DatabaseHelper.
   DatabaseHelper._internal();
 
+
+  //Database Initialization
+
+  // A getter that initializes the database if it
+  // hasn't been initialized yet, otherwise returns the existing instance.
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDb();
     return _database!;
   }
 
+  // A private method that initializes the database
   Future<Database> _initDb() async {
-    final dbPath = await getDatabasesPath();
+    //getDatabasesPath(): Retrieves the default database directory path for the app.
+    final dbPath = await getDatabasesPath();//
+    //join(dbPath, 'todo.db'): Joins the directory path with the database name todo.db.
     final path = join(dbPath, 'todo.db');
 
     return await openDatabase(
